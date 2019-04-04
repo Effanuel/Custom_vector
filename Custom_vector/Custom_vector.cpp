571efbdaa7bf2eca13bf0962cb825e9c1b0b5db2
Effanuel <etajuozas@gmail.com>
2019-04-04 12:34:53 +0300

    removed pch.h precompiled header

--- Custom_vector/Custom_vector.cpp
+++ Custom_vector/Custom_vector.cpp
@@ -1,12 +1,13 @@
 // Custom_vector.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "pch.h"
 #include <iostream>
 
 int main()
 {
     std::cout << "Hello World!\n";
+	std::cout << "Hello World!\n";
+	std::cout << "Hello World!\n";
 	std::cout << "Hello World!\n";
 }
 
