55e2676f73abd36173cf826ab29307e2b0c981ae
Effanuel <etajuozas@gmail.com>
2019-04-16 20:52:01 +0300

    Added copy constr and op=

--- Custom_vector/Vector.cpp
+++ Custom_vector/Vector.cpp
@@ -10,3 +10,17 @@ void Vector::setElem(int idx, double val) {
 	if (idx < 0 || idx >= sz) return;
 	elem[idx] = val;
 }
+
+Vector& Vector::operator=(const Vector& v) {
+
+	if (&v == this) return *this;
+
+	double* p = new double[v.sz];
+	for (int i = 0; i != v.sz; ++i) {
+		p[i] = v.elem[i];
+		delete[] elem;
+		elem = p;
+		sz = v.sz;
+		return *this;
+	}
+}
--- Custom_vector/Vector.h
+++ Custom_vector/Vector.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <algorithm>
+#include <stdexcept>
 
 class Vector {
 private:
@@ -9,10 +10,19 @@ public:
 	Vector() : sz{ 0 }, elem{ new double[sz] } {}
 	Vector(int s);
 	Vector(int s, double val);
+	Vector(const Vector& v) : elem{ new double[v.sz] }, sz{ v.sz }	{
+		for (int i = 0; i != sz; ++i) elem[i] = v.elem[i];
+	}
 	~Vector() { delete[] elem; }
 
 	int size() const { return sz; }
 	double getElem(int i) { return elem[i]; }
 
 	void setElem(int idx, double val);
+
+	double& operator[](int i) {
+		if (i < 0 || size() <= i) throw std::out_of_range{ "Vector::operator[]" };
+		return elem[i];
+	}
+	const double& operator[](int i) const { return elem[i]; }
 };
