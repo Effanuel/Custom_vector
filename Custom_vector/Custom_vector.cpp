5a600a034e6e574e70fb7cb2315b2e50107dac50
Effanuel <32594043+Effanuel@users.noreply.github.com>
2019-05-03 20:21:40 +0300

    merge

--- Custom_vector/Custom_vector.cpp
+++ Custom_vector/Custom_vector.cpp
@@ -1,12 +1,11 @@
-#include "Vector.h"
 #include <iostream>
-#include <vector>
+#include "Vector.h"
 
-int main()
-{
-	Vector a{ 10, 2, 4 };
-	Vector b{ 10, 2, 5 };
-	//std::cout << d << std::endl;
-	a.push_back(4);
-	for (auto i = 0; i != a.size(); ++i) std::cout << a[i] << std::endl;
+
+int main() {
+	Vector a{ 10, 2, 3 };
+
+	for (int i = 0; i != a.size(); ++i) {
+		std::cout << a[i] << std::endl;
+	}
 }
