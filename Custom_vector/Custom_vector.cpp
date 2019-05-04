fada9af290a61d4de6782d878716b80de8db566c
Effanuel <32594043+Effanuel@users.noreply.github.com>
2019-05-04 20:56:15 +0300

    Added templates, push_back, capacity member, clear, reserve

--- Custom_vector/Custom_vector.cpp
+++ Custom_vector/Custom_vector.cpp
@@ -4,11 +4,12 @@
 
 
 int main() {
-	Vector a{ 1, 1, 3 };
-	Vector b{ 1, 1, -10};
-	Vector c = a - b;
+	Vector<int> a{ 1, 1, 3 };
+	Vector<int> b{ 1, 1, 5};
+	Vector<int> c;
+	b.push_back(3);
 	if(b < a) std::cout << "ok" << std::endl;
-	for (int i = 0; i != c.size(); ++i) {
-		std::cout << c[i] << std::endl;
+	for (size_t i = 0; i != b.size(); ++i) {
+		std::cout << b[i] << std::endl;
 	}
 }
--- Custom_vector/Vector.h
+++ Custom_vector/Vector.h
@@ -3,43 +3,265 @@
 #include <algorithm>
 #include <stdexcept>
 
-class Vector {
+using std::size_t;
+
+template<class T> class Vector {
 private:
-	int sz;
-	double* elem;
+	T* elem;
+	size_t sz;
+	size_t cap;
+
 public:
-	Vector() : sz{ 0 }, elem{ new double[sz] } {}
+	Vector() : elem{ 0 }, sz{ 0 }, cap{ 8 }  {}
 	Vector(int s);
-	Vector(int s, double val);
+	//Vector(int s, T val);
 	Vector(const Vector& v);
 	Vector(Vector&& v);
-	Vector(std::initializer_list<double> il);
+	Vector(std::initializer_list<T> il);
 	~Vector();
 
-	int size() const { return sz; }
+	size_t size() const { return sz; }
+	size_t capacity() const { return cap; }
+
 	//void resize(int count);
-	double getElem(int i) const { return elem[i]; }
-	void setElem(int idx, double val);
+	T getElem(int i) const { return elem[i]; }
+	void setElem(int idx, T val);
 
-	Vector& operator=(const Vector&);
-	Vector& operator=(Vector&&);
+	Vector<T>& operator=(const Vector<T>&);
+	Vector<T>& operator=(Vector&&);
 
-	friend Vector operator+(const Vector&, const Vector&);
-	friend Vector operator-(const Vector&, const Vector&);
+	friend Vector<T> operator+(const Vector<T>&, const Vector<T>&);
+	friend Vector<T> operator-(const Vector<T>&, const Vector<T>&);
 
-	bool operator==(const Vector&);
-	bool operator!=(const Vector&);
-	bool operator>(const Vector&);
-	bool operator<(const Vector&);
+	bool operator==(const Vector<T>&);
+	bool operator!=(const Vector<T>&);
+	bool operator>(const Vector<T>&);
+	bool operator<(const Vector<T>&);
 
-	double& operator[](int i) {
-		if (i < 0 || size() <= i) throw std::out_of_range{ "Vector::operator[]" };
+	T& operator[](size_t i) {
+		if (i < 0 || capacity() <= i) throw std::out_of_range{ "Vector::operator[]" };
 		return elem[i];
 	}
-	const double& operator[](int i) const { return elem[i]; }
+	const T& operator[](size_t i) const { return elem[i]; }
 
-	const double& back() const;
-	double& back();
-	const double& front() const;
-	double& front();
+	const T& back() const;
+	T& back();
+	const T& front() const;
+	T& front();
+
+	void clear();
+	void reserve(int);
+
+	void push_back(const T&);
 };
+
+
+template<class T>
+void swap(T& a, T& b) {
+	T temp{ std::move(a) };
+	a = std::move(b);
+	b = std::move(temp);
+}
+
+template<class T>
+Vector<T>::Vector(const Vector& v) : elem{ new T[v.sz] }, sz{ v.sz }, cap{ v.cap }	{
+	for (size_t i = 0; i != sz; ++i) elem[i] = v.elem[i];
+}
+
+
+
+template<class T>
+Vector<T>::Vector(Vector&& v) : elem{ v.elem }, sz{ v.sz }, cap{ v.cap }	{
+	v.elem = nullptr;
+	v.sz = 0;
+	v.cap = 8;
+}
+
+
+
+
+template<class T>
+Vector<T>::Vector(std::initializer_list<T> il)
+	: elem{ new T[il.size()] },
+	sz{ static_cast<size_t>(il.size()) }, 
+	cap{ static_cast<size_t>(il.size()) }
+		{
+			std::copy(il.begin(), il.end(), elem);
+		}
+
+
+
+template<class T>
+Vector<T>::~Vector() { delete[] elem; }
+
+
+
+template<class T>
+Vector<T>::Vector(int s) : elem{ new T[s] }, sz{ s }, cap{ s } {
+	for (int i = 0; i < sz; ++i)
+		elem[i] = T();
+}
+
+
+
+//template<class T>
+//Vector<T>::Vector(int s, T val) : elem{ new T[s] }, sz{ s }, cap{ s }  {
+//	std::fill_n(elem, s, val);
+//}
+
+
+
+template<class T>
+void Vector<T>::setElem(int idx, T val) {
+	if (idx < 0 || idx >= sz) return;
+	elem[idx] = val;
+}
+
+
+
+//void Vector::resize(int count) {
+//	if (count) throw std::logic_error("Cannot resize to a negative value");
+//	sz = count;
+//	for(int i = 0; i)
+//}
+template<class T>
+Vector<T>& Vector<T>::operator=(const Vector<T> & v) {
+	if (&v == this) return *this;
+	T* p = new T[v.sz];
+	for (int i = 0; i != v.sz; ++i) {
+		p[i] = v.elem[i];
+		delete[] elem;
+		elem = p;
+		sz = v.sz;
+		return *this;
+	}
+}
+template<class T>
+Vector<T>& Vector<T>::operator=(Vector<T>&& v) {
+	if (&v == this) return *this;
+	delete[] elem;
+	elem = v.elem;
+	sz = v.sz;
+	v.elem = nullptr;
+	v.sz = 0;
+	return *this;
+}
+template<class T>
+Vector<T> operator+(const Vector<T> & a, const Vector<T> & b) {
+	if (a.size() != b.size())
+		throw std::runtime_error("Vector dydziu neatitikimas");
+	auto size = a.size();
+	Vector<T> c(size);
+	for (auto i = 0; i != a.size(); ++i)
+		c[i] = a[i] + b[i];
+	return c;
+
+}
+template<class T>
+Vector<T> operator-(const Vector<T> & a, const Vector<T> & b) {
+	if (a.size() != b.size())
+		throw std::runtime_error("Vector dydziu neatitikimas");
+	size_t size = a.size();
+	Vector<T> c( size );
+	for (size_t i = 0; i != a.size(); ++i)
+		c[i] = a[i] - b[i];
+	return c;
+
+}
+template<class T>
+bool Vector<T>::operator==(const Vector<T> & v) { // added
+	if (sz == v.size()) {
+		for (int i = 0; i != sz; ++i) {
+			if (elem[i] != v[i]) return false;
+			return true;
+		}
+	}
+	return false;
+}
+template<class T>
+bool Vector<T>::operator!=(const Vector<T> & v) {//added
+	return !operator==(v);
+}
+
+template<class T>
+bool Vector<T>::operator>(const Vector<T> & v) {
+	if (elem == v.elem && sz == v.size()) return false;
+
+
+	auto min_siz = sz < v.size() ? sz : v.size();
+	for (int i = 0; i != min_siz; ++i) {
+		if (elem[i] == v.elem[i]) continue;
+		return elem[i] > v.elem[i] ? true : false;
+
+	}
+	return min_siz == sz ? false : true; // if equal but one's size is greater
+}
+
+template<class T>
+bool Vector<T>::operator<(const Vector & v) {
+	if (elem == v.elem && sz == v.size()) return false;
+
+	auto min_siz = sz < v.size() ? sz : v.size();
+	for (int i = 0; i != min_siz; ++i) {
+		if (elem[i] == v.elem[i]) continue;
+		return elem[i] < v.elem[i] ? true : false;
+
+	}
+	return min_siz == sz ? true : false; // if equal but one's size is greater
+}
+
+
+template<class T>
+const T& Vector<T>::back() const {
+	if (sz == 0) throw std::out_of_range("Empty vector");//added
+	return elem[sz - 1];
+}
+
+template<class T>
+T& Vector<T>::back() {
+	if (sz == 0) throw std::out_of_range("Empty vector");//added
+	return elem[sz - 1];
+}
+template<class T>
+const T& Vector<T>::front() const {
+	if (sz == 0) throw std::out_of_range("Empty vector");//added
+	return elem[0];
+}
+
+template<class T>
+T& Vector<T>::front() {
+	if (sz == 0) throw std::out_of_range("Empty vector");//added
+	return elem[0];
+}
+
+template<class T>
+void Vector<T>::clear() {
+	delete[] elem;
+	sz = 0;
+	cap = 4;
+	elem = new T[cap];
+}
+template<class T>
+void Vector<T>::reserve(int mem) {
+	if (mem <= cap) return;
+
+	T* temp = new T[mem];
+
+	for (int i = 0; i < sz; ++i) {
+		temp[i] = elem[i];
+	}
+	delete[] elem;
+	cap = mem;
+	elem = temp;
+	
+}
+template<class T>
+void Vector<T>::push_back(const T& val) {
+	if (cap == 0)
+		reserve(8);
+	else if (sz == cap)
+		reserve(2 * cap);
+
+	elem[sz] = val;
+	++sz;
+}
